f0b5cc0117e92c554788d6c7ebe99325f6473e32
avanish1010 <87857791+avanish1010@users.noreply.github.com>
2021-11-19 20:48:25 +0530

    Add files via upload

---
+++ Write a Python program to reverse a string..py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def reverse_string(string):
+    return string[::-1]
+reverse_string("1234abcd")
+
+
+# In[ ]:
+
+
+
+
---
+++ _Write a Python function to sum all the numbers in a list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def addition(*numbers):
+    total=0
+    for i in numbers:
+        total+=i
+    return total
+addition(8,2,3,0,7)
+
+
+# In[ ]:
+
+
+
+
